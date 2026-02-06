47dc065c43bce1627b585c2daebe191cf7e80ef2
Austin Bullard <austinbullard@protonmail.ch>
2026-02-05 20:21:12 -0800

    add footer for deduplication

--- src/discord-client.js
+++ src/discord-client.js
@@ -1,5 +1,5 @@
-import { Client, Events, GatewayIntentBits, EmbedBuilder } from "discord.js";
-import { mapFreeEpicGames } from "./epic-games.js";
+import { Client, Events, GatewayIntentBits, EmbedBuilder } from 'discord.js';
+import { mapFreeEpicGames } from './epic-games.js';
 
 export const loginWithDiscord = () => {
     const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -9,7 +9,9 @@ export const loginWithDiscord = () => {
         // AJB TODO - extract main here
         const result = await mapFreeEpicGames();
 
-        // await sendMessage(client, result);
+        await sendMessage(client, result);
+
+        process.exit(0);
     });
 
     client.login(process.env.DISCORD_TOKEN);
@@ -18,31 +20,51 @@ export const loginWithDiscord = () => {
 };
 
 export const sendMessage = async (client, freeGames) => {
-    const channel = await client.channels.fetch("1468456945529917564");
+    const channel = await client.channels.fetch('1468456945529917564');
 
-    freeGames.map((fg) => {
+    // TODO extract to function
+    const messages = await channel.messages.fetch({ limit: 2, cache: false });
+    const previouslySentGames = messages.map((x) => {
+        const hasEmbeds = x.embeds.length;
+        if (hasEmbeds) {
+            return x.embeds[0].data.footer?.text;
+        }
+    });
+
+    const toSend = [];
+    freeGames.map(async (fg) => {
+        if (previouslySentGames.includes(fg.slug)) {
+            console.info(`dont need to send, already sent ${fg.title}`);
+            return;
+        }
+
         const exampleEmbed = new EmbedBuilder()
             .setColor(0x5bbb56)
             .setTitle(`${fg.title} is Free on Epic Games!`)
             .setURL(`https://store.epicgames.com/en-US/p/${fg.slug}`)
             .setDescription(fg.description)
             .setImage(fg.thumbnail)
-            .addFields({
-                name: "Cost",
-                value: "$0.00",
-                inline: true,
-            })
-            .addFields({
-                name: "Developer",
-                value: fg.developer,
-                inline: true,
-            })
-            .addFields({
-                name: "Status",
-                value: fg.status,
-                inline: true,
-            });
+            .addFields(
+                {
+                    name: 'Cost',
+                    value: '$0.00',
+                    inline: true,
+                },
+                {
+                    name: 'Developer',
+                    value: fg.developer,
+                    inline: true,
+                },
+                {
+                    name: 'Status',
+                    value: fg.status,
+                    inline: true,
+                }
+            )
+            .setFooter({ text: `${fg.slug}` });
 
-        channel.send({ embeds: [exampleEmbed] });
+        toSend.push(channel.send({ embeds: [exampleEmbed] }));
     });
+
+    await Promise.all(toSend);
 };
