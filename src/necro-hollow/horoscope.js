b736b2fbd23d2226649f4a5c685d1eff5a3830e8
Austin Bullard <austinbullard@protonmail.ch>
2026-04-25 22:40:52 -0700

    did not need ky

--- src/necro-hollow/horoscope.js
+++ src/necro-hollow/horoscope.js
@@ -1,12 +1,11 @@
-import ky from "ky";
-
 export const getHoroscopes = async () => {
     const dailyHoroscopeRequets = [];
     const signs = ['aries', 'taurus', 'gemini', 'cancer', 'leo', 'virgo', 'libra', 'scorpio', 'sagittarius', 'capricorn', 'aquarius', 'pisces'];
 
     for (const sign of signs) {
         const url = `https://api.api-ninjas.com/v1/horoscope?zodiac=${sign}`;
-        dailyHoroscopeRequets.push(ky.get(url).json());
+
+        dailyHoroscopeRequets.push((await fetch(url)).json());
     }
 
     return await Promise.all(dailyHoroscopeRequets);
