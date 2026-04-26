ed531e588ee106efb5fc9c01cfd5642e6cc42abc
Austin Bullard <austinbullard@protonmail.ch>
2026-04-25 22:43:32 -0700

    minor documentation

--- src/necro-hollow/index-necro.js
+++ src/necro-hollow/index-necro.js
@@ -2,6 +2,7 @@ import { loginWithDiscord } from '../discord-client.js';
 import { readyHandler } from './handler.js';
 
 const run = async () => {
+    // using https://api-ninjas.com/api/horoscope for daily horoscopes
     loginWithDiscord(process.env.DISCORD_TOKEN_NECRO, readyHandler);
 };
 
